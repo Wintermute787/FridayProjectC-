219e5acacbd13161ec39e82b31376b3122a023da
Joe Barnes <joebarnes333@gmail.com>
2019-04-26 13:57:10 -0700

    add info to read me about number tester

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -59,9 +59,9 @@ namespace WordCounter.Models
             char[] charSent = sentence.ToCharArray();
             List<int> numList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             
-            for(int i =0; i < charSent.Length; i++)
+            for(int i =0; i < charSent.Length -1; i++)
             {
-                if(charSent[i] == numList[i])
+                if (charSent[i] == numList[i])
                 {
                     return true;
                 }
